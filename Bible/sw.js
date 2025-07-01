45b06fb383e453f2ccd27ee76ab7abe6855e912c
mdudumi <martin.dudumi@gmail.com>
2025-07-01 16:08:34 +0200

    Update sw.js

--- Bible/sw.js
+++ Bible/sw.js
@@ -1,5 +1,24 @@
 const CACHE_NAME = 'biblical-planner-v1';
-const ASSETS = ['/', '/index.html', '/css/styles.css', '/js/script.js', 'json/manifest.json', '/icons/icon-192x192.png', '/icons/icon-512x512.png'];
+const ASSETS = [
+  '/',
+  '/index.html',
+  'css/styles.css',
+  'js/script.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
