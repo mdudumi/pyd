3ed8579ba0442e1b25820753653026835799bbf8
mdudumi <martin.dudumi@gmail.com>
2025-07-01 15:53:50 +0200

    Delete Bible/js/sw.js

--- Bible/js/sw.js
+++
@@ -1,6 +0,0 @@
---- **sw.js** ---
-const CACHE_NAME = 'biblical-planner-v1';
-const ASSETS = ['/', '/index.html', '/css/styles.css', '/js/script.js', 'json/manifest.json', '/icons/icon-192x192.png', '/icons/icon-512x512.png'];
-
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
